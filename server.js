a1e1cbcf9d4433dead9cc12c11623c8b5dafb3fd
Akash Sharma <officialmailakashsharma@gmail.com>
2026-04-15 10:38:25 +0530

    final ci cdx

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 // ✅ Health Check Endpoint
 app.get("/health", (req, res) => {
